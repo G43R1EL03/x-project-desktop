7bb7712743f9c5940dddef2ab28faa6a92db1d41
Alik <moisesalik01@gmail.com>
2023-10-13 17:07:36 -0500

    menu

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,186 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.PanelMenu = New System.Windows.Forms.Panel()
+        Me.PanelLogo = New System.Windows.Forms.Panel()
+        Me.PictureBoxLogo = New System.Windows.Forms.PictureBox()
+        Me.IconButtonHome = New FontAwesome.Sharp.IconButton()
+        Me.IconButton1 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton2 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton3 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton4 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton5 = New FontAwesome.Sharp.IconButton()
+        Me.PanelMenu.SuspendLayout()
+        Me.PanelLogo.SuspendLayout()
+        CType(Me.PictureBoxLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PanelMenu
+        '
+        Me.PanelMenu.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.PanelMenu.Controls.Add(Me.IconButton5)
+        Me.PanelMenu.Controls.Add(Me.IconButton4)
+        Me.PanelMenu.Controls.Add(Me.IconButton3)
+        Me.PanelMenu.Controls.Add(Me.IconButton2)
+        Me.PanelMenu.Controls.Add(Me.IconButton1)
+        Me.PanelMenu.Controls.Add(Me.IconButtonHome)
+        Me.PanelMenu.Controls.Add(Me.PanelLogo)
+        Me.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PanelMenu.Location = New System.Drawing.Point(0, 0)
+        Me.PanelMenu.Name = "PanelMenu"
+        Me.PanelMenu.Size = New System.Drawing.Size(171, 450)
+        Me.PanelMenu.TabIndex = 0
+        '
+        'PanelLogo
+        '
+        Me.PanelLogo.Controls.Add(Me.PictureBoxLogo)
+        Me.PanelLogo.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelLogo.Location = New System.Drawing.Point(0, 0)
+        Me.PanelLogo.Name = "PanelLogo"
+        Me.PanelLogo.Size = New System.Drawing.Size(171, 100)
+        Me.PanelLogo.TabIndex = 0
+        '
+        'PictureBoxLogo
+        '
+        Me.PictureBoxLogo.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBoxLogo.Image = Global.x_project_desktop.My.Resources.Resources.logo
+        Me.PictureBoxLogo.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBoxLogo.Name = "PictureBoxLogo"
+        Me.PictureBoxLogo.Size = New System.Drawing.Size(171, 100)
+        Me.PictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBoxLogo.TabIndex = 0
+        Me.PictureBoxLogo.TabStop = False
+        '
+        'IconButtonHome
+        '
+        Me.IconButtonHome.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButtonHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButtonHome.ForeColor = System.Drawing.Color.White
+        Me.IconButtonHome.IconChar = FontAwesome.Sharp.IconChar.HomeUser
+        Me.IconButtonHome.IconColor = System.Drawing.Color.White
+        Me.IconButtonHome.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButtonHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButtonHome.Location = New System.Drawing.Point(0, 100)
+        Me.IconButtonHome.Name = "IconButtonHome"
+        Me.IconButtonHome.Size = New System.Drawing.Size(171, 60)
+        Me.IconButtonHome.TabIndex = 1
+        Me.IconButtonHome.Text = "Home"
+        Me.IconButtonHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButtonHome.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButtonHome.UseVisualStyleBackColor = True
+        '
+        'IconButton1
+        '
+        Me.IconButton1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton1.ForeColor = System.Drawing.Color.White
+        Me.IconButton1.IconChar = FontAwesome.Sharp.IconChar.HomeUser
+        Me.IconButton1.IconColor = System.Drawing.Color.White
+        Me.IconButton1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton1.Location = New System.Drawing.Point(0, 160)
+        Me.IconButton1.Name = "IconButton1"
+        Me.IconButton1.Size = New System.Drawing.Size(171, 60)
+        Me.IconButton1.TabIndex = 2
+        Me.IconButton1.Text = "Home"
+        Me.IconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButton1.UseVisualStyleBackColor = True
+        '
+        'IconButton2
+        '
+        Me.IconButton2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton2.ForeColor = System.Drawing.Color.White
+        Me.IconButton2.IconChar = FontAwesome.Sharp.IconChar.HomeUser
+        Me.IconButton2.IconColor = System.Drawing.Color.White
+        Me.IconButton2.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton2.Location = New System.Drawing.Point(0, 220)
+        Me.IconButton2.Name = "IconButton2"
+        Me.IconButton2.Size = New System.Drawing.Size(171, 60)
+        Me.IconButton2.TabIndex = 3
+        Me.IconButton2.Text = "Home"
+        Me.IconButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButton2.UseVisualStyleBackColor = True
+        '
+        'IconButton3
+        '
+        Me.IconButton3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton3.ForeColor = System.Drawing.Color.White
+        Me.IconButton3.IconChar = FontAwesome.Sharp.IconChar.HomeUser
+        Me.IconButton3.IconColor = System.Drawing.Color.White
+        Me.IconButton3.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton3.Location = New System.Drawing.Point(0, 280)
+        Me.IconButton3.Name = "IconButton3"
+        Me.IconButton3.Size = New System.Drawing.Size(171, 60)
+        Me.IconButton3.TabIndex = 4
+        Me.IconButton3.Text = "Home"
+        Me.IconButton3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButton3.UseVisualStyleBackColor = True
+        '
+        'IconButton4
+        '
+        Me.IconButton4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton4.ForeColor = System.Drawing.Color.White
+        Me.IconButton4.IconChar = FontAwesome.Sharp.IconChar.HomeUser
+        Me.IconButton4.IconColor = System.Drawing.Color.White
+        Me.IconButton4.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton4.Location = New System.Drawing.Point(0, 340)
+        Me.IconButton4.Name = "IconButton4"
+        Me.IconButton4.Size = New System.Drawing.Size(171, 60)
+        Me.IconButton4.TabIndex = 5
+        Me.IconButton4.Text = "Home"
+        Me.IconButton4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButton4.UseVisualStyleBackColor = True
+        '
+        'IconButton5
+        '
+        Me.IconButton5.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton5.ForeColor = System.Drawing.Color.White
+        Me.IconButton5.IconChar = FontAwesome.Sharp.IconChar.HomeUser
+        Me.IconButton5.IconColor = System.Drawing.Color.White
+        Me.IconButton5.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton5.Location = New System.Drawing.Point(0, 400)
+        Me.IconButton5.Name = "IconButton5"
+        Me.IconButton5.Size = New System.Drawing.Size(171, 60)
+        Me.IconButton5.TabIndex = 6
+        Me.IconButton5.Text = "Home"
+        Me.IconButton5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButton5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PanelMenu)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.PanelMenu.ResumeLayout(False)
+        Me.PanelLogo.ResumeLayout(False)
+        CType(Me.PictureBoxLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents PanelMenu As Panel
+    Friend WithEvents PanelLogo As Panel
+    Friend WithEvents PictureBoxLogo As PictureBox
+    Friend WithEvents IconButtonHome As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton5 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton4 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton3 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton2 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton1 As FontAwesome.Sharp.IconButton
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
+    Private Sub IconButtonHome_Click(sender As Object, e As EventArgs) Handles IconButtonHome.Click
 
+    End Sub
 End Class
