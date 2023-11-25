9a2b6e266dd6923e59400d51325280cbe98d9d8b
crestt15 <137251344+crestt15@users.noreply.github.com>
2023-11-25 16:03:56 -0500

    Pantallas Marca y Categoria hechas

---
+++ Forms Modules/frmsInventario/FrmAgregarCategoria.vb
@@ -0,0 +1,10 @@
+﻿Public Class FrmAgregarCategoria
+
+    Private Sub FrmAgregarCategoria_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.ClientSize = New Size(pnlAgregarCategoria.Width, pnlAgregarCategoria.Height)
+    End Sub
+
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        Me.Close()
+    End Sub
+End Class
