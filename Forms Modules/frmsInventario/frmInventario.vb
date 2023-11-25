63f471635a06f76e09103f5e2bc7d243e4badaa6
Cebrian10 <aldaircebrian@hotmail.com>
2023-11-25 08:52:44 -0500

    Add
    Tabla entradas

--- Forms Modules/frmsInventario/frmInventario.vb
+++ Forms Modules/frmsInventario/frmInventario.vb
@@ -1,12 +1,11 @@
 ﻿Public Class frmInventario
     Private Sub frmInventario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.BackColor = RGBColors.Purple
-
         hideSubMenu()
     End Sub
 
     Private Sub hideSubMenu()
-        panelSplitButonOpciones.Visible = False
+        panelSplitButonInventario.Visible = False
+        panelSplitButonEntradas.Visible = False
     End Sub
 
     Private Sub showSubMenu(submenu As Panel)
@@ -18,7 +17,11 @@
         End If
     End Sub
 
-    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
-        showSubMenu(panelSplitButonOpciones)
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregarInventario.Click
+        showSubMenu(panelSplitButonInventario)
+    End Sub
+
+    Private Sub btnAgregarEntradas_Click(sender As Object, e As EventArgs) Handles btnAgregarEntradas.Click
+        showSubMenu(panelSplitButonEntradas)
     End Sub
 End Class
