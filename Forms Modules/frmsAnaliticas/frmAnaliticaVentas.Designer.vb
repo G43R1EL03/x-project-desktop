836a857c845b8424e77f5e9696c8fd930a991302
Pablo Palacios <ppalacios500@gmail.com>
2023-11-28 22:11:18 -0500

    PATRON DAO EJEMPLO EN MODULO DE ANALITICA

---
+++ Forms Modules/frmsAnaliticas/frmAnaliticaVentas.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAnaliticaVentas
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvVentasPorID = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvVentasPorID, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvVentasPorID
+        '
+        Me.dgvVentasPorID.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvVentasPorID.Location = New System.Drawing.Point(89, 79)
+        Me.dgvVentasPorID.Name = "dgvVentasPorID"
+        Me.dgvVentasPorID.Size = New System.Drawing.Size(622, 292)
+        Me.dgvVentasPorID.TabIndex = 1
+        '
+        'frmAnaliticaVentas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dgvVentasPorID)
+        Me.Name = "frmAnaliticaVentas"
+        Me.Text = "frmAnaliticaVentas"
+        CType(Me.dgvVentasPorID, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dgvVentasPorID As DataGridView
+End Class
