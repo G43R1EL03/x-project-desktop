b5350ac2bf36dd288a6a203411c7b3bf8feaa148
Marcos Candanedo <01candanedo@gmail.com>
2023-12-01 23:48:53 -0500

    .

--- Interfaces Modules/tickets/ticketsInterfaces.vb
+++ Interfaces Modules/tickets/ticketsInterfaces.vb
@@ -1,7 +1,4 @@
 ﻿Public Interface ticketsInterfaces
     Function ObtenerReclamos() As DataTable
-
-    Function InsertarTicket(ByVal categoriaId As Integer, ByVal descripcion As String, ByVal prioridadId As Integer, ByVal rutaImagen As String) As Integer
-
-    Function ObtenerCategorias() As DataTable
+    Function InsertarTicket() As Integer
 End Interface
