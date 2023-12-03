ff4174ef1c156fd8e947f7319894b48689c7cae6
Pablo Palacios <ppalacios500@gmail.com>
2023-12-03 17:23:23 -0500

    ADD inventario de productos con filtro ADD Modulo para exportar tabla de inventario a excel

---
+++ Class Modules/analitica/analiticaReporteInventarioDAO.vb
@@ -0,0 +1,82 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class analiticaReporteInventarioDAO
+    Implements analitycInterfaces
+
+    Private myConnection As MySqlConnection
+
+    Public Sub New(myConnection As MySqlConnection)
+        Me.myConnection = myConnection
+    End Sub
+
+    Public Function ObtenerInventarioSegmentado(id_marca As Integer, id_categoria As Integer) As DataTable Implements analitycInterfaces.ObtenerInventarioSegmentado
+        Try
+            Dim marcaParam As Object = If(id_marca = 0, DBNull.Value, id_marca)
+            Dim categoriaParam As Object = If(id_categoria = 0, DBNull.Value, id_categoria)
+            Using glCommand As New MySqlCommand("SP_ObtenerReporteInventario", myConnectionDB)
+                glCommand.Parameters.AddWithValue("@marca_id_param", marcaParam)
+                glCommand.Parameters.AddWithValue("@categoria_id_param", categoriaParam)
+                glCommand.CommandTimeout = 0
+                glCommand.CommandType = CommandType.StoredProcedure
+
+                Using adapter As New MySqlDataAdapter(glCommand)
+                    Dim dataTable As New DataTable()
+                    adapter.Fill(dataTable)
+                    Return dataTable
+                End Using
+            End Using
+        Catch ex As Exception
+            Throw New Exception("Error al Obtener el Inventario", ex)
+            Throw New NotImplementedException()
+        End Try
+    End Function
+
+    Public Function ObtenerMarcas() As DataTable Implements analitycInterfaces.ObtenerMarcas
+        Try
+            Using glCommand As New MySqlCommand("SP_ObtenerMarcas", myConnectionDB)
+                glCommand.CommandTimeout = 0
+                glCommand.CommandType = CommandType.StoredProcedure
+
+                Using adapter As New MySqlDataAdapter(glCommand)
+                    Dim dataTable As New DataTable()
+                    adapter.Fill(dataTable)
+                    Return dataTable
+                End Using
+            End Using
+        Catch ex As Exception
+            Throw New Exception("Error al obtener las marcas", ex)
+        End Try
+
+    End Function
+
+    Public Function ObtenerCategorias() As DataTable Implements analitycInterfaces.ObtenerCategorias
+        Try
+            Using glCommand As New MySqlCommand("SP_ObtenerCategorias", myConnectionDB)
+                glCommand.CommandTimeout = 0
+                glCommand.CommandType = CommandType.StoredProcedure
+
+                Using adapter As New MySqlDataAdapter(glCommand)
+                    Dim dataTable As New DataTable()
+                    adapter.Fill(dataTable)
+                    Return dataTable
+                End Using
+            End Using
+        Catch ex As Exception
+            Throw New Exception("Error al obtener las categorías", ex)
+        End Try
+    End Function
+
+
+
+    Public Function ExportarExcel(dgv As DataGridView) As Integer Implements analitycInterfaces.ExportarExcel
+        Try
+            Dim resultado = 0
+            ExportarArchivos.ExportarAExcel(dgv)
+            Return resultado
+        Catch ex As Exception
+            Throw New Exception("Algo fallo en la exportación a Excel", ex)
+            MsgBox(ex.Message)
+        End Try
+    End Function
+
+End Class
--- Forms Modules/frmsAnaliticas/reportes/frmReporteInventario.vb
+++ Forms Modules/frmsAnaliticas/reportes/frmReporteInventario.vb
@@ -1,3 +1,87 @@
-﻿Public Class frmReporteInventario
+﻿Imports FontAwesome.Sharp
+Imports MySql.Data.MySqlClient
+
+Public Class frmReporteInventario
+    Private analiticaReporteInventarioDao As analitycInterfaces
+    Dim id_marca As Integer
+    Dim id_categoria As Integer
+
+    Public Sub New(analiticaReporteInventarioDAO As analitycInterfaces)
+        InitializeComponent()
+        Me.analiticaReporteInventarioDao = analiticaReporteInventarioDAO
+    End Sub
+
+    Private Sub frmReporteInventario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Dock = DockStyle.Fill
+        cargarInventario(id_marca, id_categoria)
+        cboxMarcas.SelectedIndex = -1
+        cboxCategorias.SelectedIndex = -1
+
+    End Sub
+
+    Private Sub cargarInventario(id_marca, id_categoria)
+        cargarCategorias()
+        cargarMarcas()
+        If cboxCategorias.SelectedValue = Nothing Then
+            id_categoria = 0
+        End If
+
+        If cboxMarcas.SelectedValue = Nothing Then
+            id_marca = 0
+        End If
+
+        Dim inventarioDataTable As DataTable = analiticaReporteInventarioDao.ObtenerInventarioSegmentado(id_marca, id_categoria)
+        Me.Dock = DockStyle.Fill
+        dgvInventarioReporte.DataSource = inventarioDataTable
+        dgvInventarioReporte.Columns("precio_unit").HeaderText = "Precio Unitario"
+        dgvInventarioReporte.Columns("cantidad_por_cajas").HeaderText = "Cant. por Cajas"
+        dgvInventarioReporte.Columns("punto_reorden").HeaderText = "Punto de reorden"
+        dgvInventarioReporte.Columns("cantidad_cajas").HeaderText = "Cant. de Cajas"
+        dgvInventarioReporte.Columns("nombre_marca").HeaderText = "Marca"
+        dgvInventarioReporte.Columns("nombre_categoria").HeaderText = "Categoría"
+
+        lblCantProductos.Text = contabilizar("nombre", inventarioDataTable)
+        lblCantCategorias.Text = contabilizar("nombre_categoria", inventarioDataTable)
+        lblCantMarcas.Text = contabilizar("nombre_marca", inventarioDataTable)
+    End Sub
+
+    Private Sub cargarMarcas()
+
+        Dim categoriasDataTable As DataTable = analiticaReporteInventarioDao.ObtenerCategorias
+        With cboxCategorias
+            .DataSource = categoriasDataTable
+            .ValueMember = "id_categoria"
+            .DisplayMember = "nombre"
+        End With
+
+    End Sub
+
+    Private Sub cargarCategorias()
+        Dim marcasDataTable As DataTable = analiticaReporteInventarioDao.ObtenerMarcas()
+        With cboxMarcas
+            .DataSource = marcasDataTable
+            .ValueMember = "id_marca"
+            .DisplayMember = "nombre"
+        End With
+
+    End Sub
+
+    Function contabilizar(columna As String, inventario As DataTable)
+        Dim cantidad As Integer = inventario.AsEnumerable().
+                                 Select(Function(row) row.Field(Of String)(columna)).
+                                 Distinct().
+                                 Count()
+        Return cantidad
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cargarInventario(cboxMarcas.SelectedValue, cboxCategorias.SelectedValue)
+        cboxMarcas.SelectedIndex = -1
+        cboxCategorias.SelectedIndex = -1
+    End Sub
+
+    Private Sub btnExportarExcel_Click(sender As Object, e As EventArgs) Handles btnExportarExcel.Click
+        analiticaReporteInventarioDao.ExportarExcel(dgvInventarioReporte)
+    End Sub
 
 End Class
