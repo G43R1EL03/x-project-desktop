bd52a096644be16d1391194aed32eca0874a25ab
JeanMurgas <jeancamurgas30@gmail.com>
2023-12-03 10:31:46 -0500

    update

--- Forms Modules/frmsInventario/FrmEditarProducto.vb
+++ Forms Modules/frmsInventario/FrmEditarProducto.vb
@@ -1,3 +1,9 @@
 ﻿Public Class FrmEditarProducto
+    Private Sub FrmEditarProducto_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub IconMenuItem1_Click(sender As Object, e As EventArgs)
+
+    End Sub
 End Class
