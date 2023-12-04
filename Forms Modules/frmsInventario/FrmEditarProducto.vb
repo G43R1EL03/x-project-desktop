e143bc33db9a39ae844f547f3e5a098b389308da
Cebrian10 <aldaircebrian@hotmail.com>
2023-12-04 07:22:11 -0500

    git revert merge JeanMurgas

--- Forms Modules/frmsInventario/FrmEditarProducto.vb
+++ Forms Modules/frmsInventario/FrmEditarProducto.vb
@@ -1,9 +1,3 @@
 ﻿Public Class FrmEditarProducto
-    Private Sub FrmEditarProducto_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub IconMenuItem1_Click(sender As Object, e As EventArgs)
-
-    End Sub
 End Class
