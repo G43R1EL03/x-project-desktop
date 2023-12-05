8f183702ecf1d3752e442b0ed1a2ead024a33306
Cebrian10 <aldaircebrian@hotmail.com>
2023-12-05 15:19:36 -0500

    Subiendo Procedimientos

---
+++ Store Procedures/inventario/SP_Categorias.sql
@@ -0,0 +1,34 @@
+-- Procedimiento para Ver Categorias
+DELIMITER $$
+CREATE PROCEDURE SP_VerCategorias()
+BEGIN
+
+END $$
+DELIMITER ;
+
+
+-- Procedimiento para Insertar Categorias
+DELIMITER $$
+CREATE PROCEDURE SP_InsertarCategorias()
+BEGIN
+
+END $$
+DELIMITER ;
+
+
+-- Procedimiento para Eliminar Categorias
+DELIMITER $$
+CREATE PROCEDURE SP_EliminarCategorias()
+BEGIN
+
+END $$
+DELIMITER ;
+
+
+-- Procedimiento para Actualizar Categorias
+DELIMITER $$
+CREATE PROCEDURE SP_ActualizarCategoria()
+BEGIN
+
+END $$
+DELIMITER ;
