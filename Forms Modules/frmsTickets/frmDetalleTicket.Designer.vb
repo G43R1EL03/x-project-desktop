3d25128c472ceae58780c1c8ad74a7a80e260f09
Marcos Candanedo <01candanedo@gmail.com>
2023-12-05 18:47:53 -0500

    Funcionalidades terminadas Tickets y Ayuda parcialmente, falta solo diseño

--- Forms Modules/frmsTickets/frmDetalleTicket.Designer.vb
+++ Forms Modules/frmsTickets/frmDetalleTicket.Designer.vb
@@ -165,8 +165,6 @@ Partial Class frmDetalleTicket
     Friend WithEvents pbEvidencia As PictureBox
     Friend WithEvents cboCambiarEstado As ComboBox
     Friend WithEvents btnCambiarEstado As FontAwesome.Sharp.IconButton
-<<<<<<< HEAD
+
 End Class
-=======
-End Class
->>>>>>> origin/marcos-candanedo
+
