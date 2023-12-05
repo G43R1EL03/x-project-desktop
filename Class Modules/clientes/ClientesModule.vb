873c036ef6044c02de4d123cac1eaefc91a13141
Bryan <32210881+Bryan0502@users.noreply.github.com>
2023-12-05 08:15:49 -0500

    Listar y Editar usarios, empresas, clientes

---
+++ Class Modules/clientes/ClientesModule.vb
@@ -0,0 +1,288 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Data
+Imports System.Windows.Documents
+
+Module ClientesModule
+
+    Public Function ObtenerUsuarios() As DataTable
+        Dim dtUsuarios As New DataTable()
+
+        Try
+            ' Abre la conexión a la base de datos
+            DB_Conecction.conexionDB()
+            DB_Conecction.myConnectionDB.Open()
+
+            ' Crea el comando para ejecutar el procedimiento almacenado
+            Dim cmd As New MySqlCommand("sp_ListarUsuarios", DB_Conecction.myConnectionDB)
+            cmd.CommandType = CommandType.StoredProcedure
+
+            ' Ejecuta el comando y llena el DataTable con los resultados
+            Dim adapter As New MySqlDataAdapter(cmd)
+            adapter.Fill(dtUsuarios)
+        Catch ex As Exception
+            ' Maneja las excepciones aquí según tu lógica
+            MsgBox("Error al obtener usuarios: " & ex.Message)
+        Finally
+            ' Cierra la conexión
+            DB_Conecction.myConnectionDB.Close()
+        End Try
+
+        Return dtUsuarios
+    End Function
+
+    Public Function ObtenerEmpresas() As DataTable
+        Dim dtEmpresas As New DataTable()
+
+        Try
+            ' Abre la conexión a la base de datos
+            DB_Conecction.conexionDB()
+            DB_Conecction.myConnectionDB.Open()
+
+            ' Crea el comando para ejecutar el procedimiento almacenado
+            Dim cmd As New MySqlCommand("sp_ListarEmpresas", DB_Conecction.myConnectionDB)
+            cmd.CommandType = CommandType.StoredProcedure
+
+            ' Ejecuta el comando y llena el DataTable con los resultados
+            Dim adapter As New MySqlDataAdapter(cmd)
+            adapter.Fill(dtEmpresas)
+        Catch ex As Exception
+            ' Maneja las excepciones aquí según tu lógica
+            MsgBox("Error al obtener empresas: " & ex.Message)
+        Finally
+            ' Cierra la conexión
+            DB_Conecction.myConnectionDB.Close()
+        End Try
+
+        Return dtEmpresas
+    End Function
+
+
+    Public Function ObtenerClientes() As DataTable
+        Dim dtClientes As New DataTable()
+
+        Try
+            ' Abre la conexión a la base de datos
+            DB_Conecction.conexionDB()
+            DB_Conecction.myConnectionDB.Open()
+
+            ' Crea el comando para ejecutar el procedimiento almacenado
+            Dim cmd As New MySqlCommand("sp_ListarClientes", DB_Conecction.myConnectionDB)
+            cmd.CommandType = CommandType.StoredProcedure
+
+            ' Ejecuta el comando y llena el DataTable con los resultados
+            Dim adapter As New MySqlDataAdapter(cmd)
+            adapter.Fill(dtClientes)
+        Catch ex As Exception
+            ' Maneja las excepciones aquí según tu lógica
+            MsgBox("Error al obtener clientes: " & ex.Message)
+        Finally
+            ' Cierra la conexión
+            DB_Conecction.myConnectionDB.Close()
+        End Try
+
+        Return dtClientes
+    End Function
+
+
+    Public Sub EliminarUsuarioPorCedula(cedula As String)
+        Try
+            ' Abre la conexión a la base de datos
+            DB_Conecction.conexionDB()
+            DB_Conecction.myConnectionDB.Open()
+
+            ' Crea el comando para ejecutar el procedimiento almacenado
+            Dim cmd As New MySqlCommand("sp_EliminarUsuario", DB_Conecction.myConnectionDB)
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("@pCedula", cedula)
+
+            ' Ejecuta el comando
+            cmd.ExecuteNonQuery()
+        Catch ex As Exception
+            ' Maneja las excepciones aquí según tu lógica
+            MsgBox("Error al eliminar usuario: " & ex.Message)
+        Finally
+            ' Cierra la conexión
+            DB_Conecction.myConnectionDB.Close()
+        End Try
+    End Sub
+
+    Public Sub EliminarEmpresaPorRUC(ruc As String)
+        Try
+            ' Abre la conexión a la base de datos
+            DB_Conecction.conexionDB()
+            DB_Conecction.myConnectionDB.Open()
+
+            ' Crea el comando para ejecutar el procedimiento almacenado
+            Dim cmd As New MySqlCommand("sp_EliminarEmpresa", DB_Conecction.myConnectionDB)
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("@pRUC", ruc)
+
+            ' Ejecuta el comando
+            cmd.ExecuteNonQuery()
+        Catch ex As MySql.Data.MySqlClient.MySqlException
+            ' Maneja las excepciones aquí según tu lógica
+            If ex.Number = 1451 Then ' Número de error para restricción de clave externa
+                MsgBox("No se puede eliminar la empresa porque tiene clientes activos.", MsgBoxStyle.Information)
+            Else
+                MsgBox("Error al eliminar empresa: " & ex.Message)
+            End If
+        Finally
+            ' Cierra la conexión
+            DB_Conecction.myConnectionDB.Close()
+        End Try
+    End Sub
+
+    Public Sub EliminarClientePorCedula(cedula As String)
+        Try
+            ' Abre la conexión a la base de datos
+            DB_Conecction.conexionDB()
+            DB_Conecction.myConnectionDB.Open()
+
+            ' Crea el comando para ejecutar el procedimiento almacenado
+            Dim cmd As New MySqlCommand("sp_EliminarCliente", DB_Conecction.myConnectionDB)
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("@pCedula", cedula)
+
+            ' Ejecuta el comando
+            cmd.ExecuteNonQuery()
+        Catch ex As MySql.Data.MySqlClient.MySqlException
+            ' Maneja las excepciones aquí según tu lógica
+            If ex.Number = 1451 Then ' Número de error para restricción de clave externa
+                MsgBox("No se puede eliminar el cliente porque tiene direcciones a su nombre.", MsgBoxStyle.Information)
+            Else
+                MsgBox("Error al eliminar cliente: " & ex.Message)
+            End If
+        Finally
+            ' Cierra la conexión
+            DB_Conecction.myConnectionDB.Close()
+        End Try
+    End Sub
+
+    Public Function ObtenerUsuarioPorCedula(cedula As String) As DataRow
+        Dim dtUsuario As DataTable = ObtenerUsuarios()
+        Dim filtro As String = $"Cedula = '{cedula}'"
+        Dim resultados() As DataRow = dtUsuario.Select(filtro)
+
+        If resultados.Length > 0 Then
+            Return resultados(0)
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Public Sub ActualizarUsuario(cedula As String, nuevosDatos As Dictionary(Of String, Object))
+        Try
+            DB_Conecction.conexionDB()
+            DB_Conecction.myConnectionDB.Open()
+
+            Dim cmd As New MySqlCommand("sp_ActualizarUsuario", DB_Conecction.myConnectionDB)
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("@pCedula", cedula)
+
+            For Each kvp As KeyValuePair(Of String, Object) In nuevosDatos
+                cmd.Parameters.AddWithValue($"@p{kvp.Key}", kvp.Value)
+            Next
+
+            cmd.ExecuteNonQuery()
+        Catch ex As Exception
+            MsgBox("Error al actualizar usuario: " & ex.Message)
+        Finally
+            DB_Conecction.myConnectionDB.Close()
+        End Try
+    End Sub
+
+    Public Function ObtenerEmpresaPorRUC(ruc As String) As DataRow
+        Dim dtEmpresa As DataTable = ObtenerEmpresas()
+        Dim filtro As String = $"RUC = '{ruc}'"
+        Dim resultados() As DataRow = dtEmpresa.Select(filtro)
+
+        If resultados.Length > 0 Then
+            Return resultados(0)
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Public Sub ActualizarEmpresa(ruc As String, nuevosDatos As Dictionary(Of String, Object))
+        Try
+            DB_Conecction.conexionDB()
+            DB_Conecction.myConnectionDB.Open()
+
+            Dim cmd As New MySqlCommand("sp_ActualizarEmpresa", DB_Conecction.myConnectionDB)
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("@pRUC", ruc)
+
+            For Each kvp As KeyValuePair(Of String, Object) In nuevosDatos
+                cmd.Parameters.AddWithValue($"@p{kvp.Key}", kvp.Value)
+            Next
+
+            cmd.ExecuteNonQuery()
+        Catch ex As Exception
+            MsgBox("Error al actualizar empresa: " & ex.Message)
+        Finally
+            DB_Conecction.myConnectionDB.Close()
+        End Try
+    End Sub
+
+    Public Sub ActualizarCliente(cedula As String, nuevosDatos As Dictionary(Of String, Object))
+        Try
+            DB_Conecction.conexionDB()
+            DB_Conecction.myConnectionDB.Open()
+
+            Dim cmd As New MySqlCommand("sp_ActualizarCliente", DB_Conecction.myConnectionDB)
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("@pCedula", cedula)
+            cmd.Parameters.Clear()
+
+            For Each kvp As KeyValuePair(Of String, Object) In nuevosDatos
+                cmd.Parameters.AddWithValue($"@p{kvp.Key}", kvp.Value)
+            Next
+
+            cmd.ExecuteNonQuery()
+        Catch ex As Exception
+            MsgBox("Error al actualizar empresa: " & ex.Message)
+        Finally
+            DB_Conecction.myConnectionDB.Close()
+        End Try
+    End Sub
+
+
+
+    Public Function ObtenerClientePorCedula(cedula As String) As DataRow
+        Dim clienteData As New DataTable()
+
+        Try
+            ' Abre la conexión a la base de datos
+            DB_Conecction.conexionDB()
+            DB_Conecction.myConnectionDB.Open()
+
+            ' Crea el comando para ejecutar el procedimiento almacenado
+            Dim cmd As New MySqlCommand("sp_ObtenerClientePorCedula", DB_Conecction.myConnectionDB)
+            cmd.CommandType = CommandType.StoredProcedure
+
+            ' Agrega el parámetro al comando
+            cmd.Parameters.AddWithValue("@pCedula", cedula)
+
+            ' Ejecuta el comando y llena el DataTable con los resultados
+            Dim adapter As New MySqlDataAdapter(cmd)
+            adapter.Fill(clienteData)
+
+            ' Verifica si se encontraron resultados y devuelve la primera fila
+            If clienteData.Rows.Count > 0 Then
+                Return clienteData.Rows(0)
+            Else
+                Return Nothing
+            End If
+        Catch ex As Exception
+            ' Maneja las excepciones aquí según tu lógica
+            MsgBox("Error al obtener cliente por cédula: " & ex.Message)
+            Return Nothing
+        Finally
+            ' Cierra la conexión
+            DB_Conecction.myConnectionDB.Close()
+        End Try
+    End Function
+
+End Module
+
+
---
+++ Forms Modules/frmsClientes/frmEditarUsuario.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEditarUsuario
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtCorreo = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtDetalles = New System.Windows.Forms.TextBox()
+        Me.cboRol = New System.Windows.Forms.ComboBox()
+        Me.cboGenero = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(280, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(204, 39)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Editar Datos"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(122, 105)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 1
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(122, 157)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(100, 20)
+        Me.txtApellido.TabIndex = 2
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.Location = New System.Drawing.Point(343, 343)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.TabIndex = 3
+        Me.btnAceptar.Text = "Aceptar"
+        Me.btnAceptar.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(26, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Nombre:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(26, 157)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Apellido:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(26, 209)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Correo:"
+        '
+        'txtCorreo
+        '
+        Me.txtCorreo.Location = New System.Drawing.Point(122, 209)
+        Me.txtCorreo.Name = "txtCorreo"
+        Me.txtCorreo.Size = New System.Drawing.Size(100, 20)
+        Me.txtCorreo.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(26, 254)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 20)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Password:"
+        '
+        'txtPass
+        '
+        Me.txtPass.Location = New System.Drawing.Point(122, 254)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.Size = New System.Drawing.Size(100, 20)
+        Me.txtPass.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(26, 302)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(37, 20)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Rol:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(406, 105)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(67, 20)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Genero:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(406, 157)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(75, 20)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Telefono:"
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(502, 157)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(100, 20)
+        Me.txtTelefono.TabIndex = 14
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(406, 207)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(71, 20)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Detalles:"
+        '
+        'txtDetalles
+        '
+        Me.txtDetalles.Location = New System.Drawing.Point(502, 207)
+        Me.txtDetalles.Name = "txtDetalles"
+        Me.txtDetalles.Size = New System.Drawing.Size(100, 20)
+        Me.txtDetalles.TabIndex = 16
+        '
+        'cboRol
+        '
+        Me.cboRol.FormattingEnabled = True
+        Me.cboRol.Location = New System.Drawing.Point(122, 304)
+        Me.cboRol.Name = "cboRol"
+        Me.cboRol.Size = New System.Drawing.Size(121, 21)
+        Me.cboRol.TabIndex = 18
+        '
+        'cboGenero
+        '
+        Me.cboGenero.FormattingEnabled = True
+        Me.cboGenero.Location = New System.Drawing.Point(502, 105)
+        Me.cboGenero.Name = "cboGenero"
+        Me.cboGenero.Size = New System.Drawing.Size(121, 21)
+        Me.cboGenero.TabIndex = 19
+        '
+        'frmEditarUsuario
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cboGenero)
+        Me.Controls.Add(Me.cboRol)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtDetalles)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtCorreo)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmEditarUsuario"
+        Me.Text = "frmEditarUsuario"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents btnAceptar As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtCorreo As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtDetalles As TextBox
+    Friend WithEvents cboRol As ComboBox
+    Friend WithEvents cboGenero As ComboBox
+End Class
